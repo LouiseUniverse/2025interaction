f68be77747bef2cb435dab6918bfbbddef589109
LouiseUniverse <145088359+LouiseUniverse@users.noreply.github.com>
2025-09-22 15:45:05 +0800

    week03

---
+++ week03-1/week03-1.ino
@@ -0,0 +1,18 @@
+//week03_1_pinMode(8,OUTPUT)_tone(8,800,200)_delay(1000)
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8,OUTPUT);
+  tone(8,800,200);
+  delay(1000);
+  pinMode(8,OUTPUT);
+  tone(8,800,200);
+  delay(1000);
+  pinMode(8,OUTPUT);
+  tone(8,800,200);
+  delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
